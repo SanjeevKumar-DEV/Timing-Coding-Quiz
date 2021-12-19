bc65fcec4225c107df9a0414f6d2f6d38bc73029
Sanjeev Kumar <sanjeevkumar@me.com>
2021-12-19 12:10:59 +1100

    Minor enhancement to stats screen and changed screenshots for readme file for stats screen statsScreen2.png.

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,7 +188,7 @@ function prepareToDisplayForHighScoreStats() {
     //saveScoresInLocalStorage();
     document.querySelector('header section.timeAndScore').setAttribute('style', 'display : none');
     var scoreHeaderMessageH2Tag = document.querySelector('header .question.scoreMessage');
-    scoreHeaderMessageH2Tag.setAttribute('style', 'display : block; height: 40px; font-size : 35px;');
+    scoreHeaderMessageH2Tag.setAttribute('style', 'display : block; height: 40px; margin: 0px; margin-top: 80px; font-size : 35px;');
     scoreHeaderMessageH2Tag.textContent = "Highscores";
     document.getElementById('yourScoreKeeper').setAttribute('style', 'display : none');
     document.getElementById('yourCredentials').setAttribute('style', 'display : none');
